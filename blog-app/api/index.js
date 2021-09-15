3e4594ffea8246f08c068914525210031362d30a
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-09-16 01:40:32 +0530

    Implemented REST api for file uploading

--- blog-app/api/index.js
+++ blog-app/api/index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-
+const catRoute = require("./routes/categories");
+const multer = require("multer");
 dotenv.config();
 
 mongoose
@@ -16,10 +17,27 @@ mongoose
   .catch((error) => {
     console.log("Error while connecting to Database");
   });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", catRoute);
 app.listen("5000", () => {
   console.log("Backend server running");
 });
